e51c44870644458ace0da8361538ba70c87a6cc9
miura0617 <j.miura0617@gmail.com>
2022-09-12 00:10:22 +0900

    next.jsとreactstrapのファーストサンプル

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+
+export default class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+        return (
+            <>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </>
+        );
+    }
+};
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Alert, Button } from "reactstrap";
+
+const index = () => {
+    return (
+        <div>
+            <div>
+                <Alert color="primary">Hello Project</Alert>
+                <Button color="primary">Hello from nextjs</Button>
+            </div>
+        </div>
+    );
+}
+
+export default index;
